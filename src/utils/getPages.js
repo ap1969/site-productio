299609ee11f2d42c1c0e35a14726daa3a84c455d
Andrew Peacock <andrewpeacock1969@gmail.com>
2020-04-21 15:44:00 +0000

    Debug display_posts

--- src/utils/getPages.js
+++ src/utils/getPages.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
 
-export default function(pages, folderPath) {
+export default function(pages, folderPath, ignoreDrafts = false) {
     // TODO: resolve relative paths relative to current page
     folderPath = folderPath.replace(/^\//, '');
     return _.filter(pages, {relativeDir: folderPath});
