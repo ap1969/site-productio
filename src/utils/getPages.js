ec29fd7254a3a5a8453790bd28644d0ad6eea2e6
Andrew <andrewpeacock1969@gmail.com>
2020-04-21 20:52:08 +0100

    debug pages in blog

--- src/utils/getPages.js
+++ src/utils/getPages.js
@@ -3,5 +3,6 @@ import _ from "lodash";
 export default function(pages, folderPath, includeDrafts = false) {
     // TODO: resolve relative paths relative to current page
     folderPath = folderPath.replace(/^\//, '');
+        console.log("Pages", pages);
     return _.filter(pages, {relativeDir: folderPath, draft: includeDrafts});
 }
