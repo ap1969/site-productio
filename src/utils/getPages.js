7cf232c6bada23682de47284a71ed14d4faf9f7c
Andrew <andrewpeacock1969@gmail.com>
2020-04-21 22:19:42 +0100

    Hard-code drafts filter

--- src/utils/getPages.js
+++ src/utils/getPages.js
@@ -1,12 +1,12 @@
 import _ from "lodash";
 
-export default function(pages, folderPath, includeDrafts = false) {
+export default function(pages, folderPath) {
     // TODO: resolve relative paths relative to current page
     folderPath = folderPath.replace(/^\//, '');
     console.log("Pages", pages);
     //return _.filter(pages, {relativeDir: folderPath, frontmatter.draft: includeDrafts});
      var pagesOut = _.filter(pages, function(page) {
-        return (page.relativeDir == folderPath && page.frontmatter.draft != includeDrafts);
+        return (page.relativeDir == folderPath && page.frontmatter.draft != true);
     });
     console.log("PagesOut", pagesOut);
     return pagesOut;
